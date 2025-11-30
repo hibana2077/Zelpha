f3dd6a430283874be1cb8b1394ac3c9fe6ce2677
hibana2077 <hibana2077@gmail.com>
2025-11-30 22:45:42 +1000

    feat: add evaluation scripts E007A.sh, E007B.sh, E007C.sh, E007D.sh, E007E.sh, E008A.sh, E008B.sh, E008C.sh, E008D.sh, and E008E.sh for densenetblur121d model training with updated parameters

---
+++ script/EVAL/E007E.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1            
+#PBS -l ncpus=12            
+#PBS -l mem=32GB           
+#PBS -l walltime=10:00:00  
+#PBS -l wd                  
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+
+source /scratch/rp06/sl5952/Zelpha/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/Zelpha/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+# Run training with E007E (N variant)
+python3 src/train.py \
+    --model_name densenetblur121d.ra_in1k \
+    --num_prototypes 1 \
+    --beta 0.01 \
+    --margin 1.0 \
+    --image_size 224 \
+    --seed 666 \
+    --no_lipschitz \
+    --no_scale_pooling \
+    --linear_epochs 50 --finetune_epochs 0 >> logs/E007E.log 2>&1
