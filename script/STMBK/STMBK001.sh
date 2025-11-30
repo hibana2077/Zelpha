3ef750f4ac3d2c8d51f2a2b4404e42c528a7e8f2
hibana2077 <hibana2077@gmail.com>
2025-11-30 10:37:42 +1000

    fix: Update image_size parameter in STMBK001 script for training consistency

--- script/STMBK/STMBK001.sh
+++ script/STMBK/STMBK001.sh
@@ -21,5 +21,5 @@ python3 src/train.py \
     --num_prototypes 1 \
     --beta 0.01 \
     --margin 0.5 \
-    --image_size 256 \
+    --image_size 224 \
     --linear_epochs 50 --finetune_epochs 50 >> logs/STMBK001.log 2>&1
