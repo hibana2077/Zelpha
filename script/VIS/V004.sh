15a123ae0a76addf96defdd06f81177174c0dc7c
hibana2077 <hibana2077@gmail.com>
2025-12-20 12:31:07 +1000

    feat: add V004 training script for model training and visualization

---
+++ script/VIS/V004.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuhopper
+#PBS -l ngpus=1            
+#PBS -l ncpus=12            
+#PBS -l mem=32GB           
+#PBS -l walltime=10:00:00  
+#PBS -l wd                  
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+
+source /scratch/rp06/sl5952/Zelpha/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/Zelpha/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+# Run training with V001
+python3 src/train.py \
+    --model_name repvgg_a1.rvgg_in1k \
+    --num_prototypes 1 \
+    --beta 0.01 \
+    --margin 1.0 \
+    --image_size 224 \
+    --seed 444 \
+    --save_dir outputs/V004P \
+    --linear_epochs 50 --finetune_epochs 50 >> logs/V004.log 2>&1
+
+python3 src/train.py \
+    --model_name repvgg_a1.rvgg_in1k \
+    --num_prototypes 1 \
+    --beta 0.01 \
+    --margin 1.0 \
+    --image_size 224 \
+    --seed 222 \
+    --no_lipschitz \
+    --no_scale_pooling \
+    --save_dir outputs/V004B \
+    --linear_epochs 50 --finetune_epochs 1 >> logs/V004.log 2>&1
+
+python3 -m src.vis \
+  --ckpt_zelpha outputs/V004P/best_proto.pt \
+  --ckpt_base   outputs/V004B/best_proto.pt \
+  --dataset UC_Merced \
+  --batch_size 64 \
+  --image_size 256 \
+  --model_name zelpha \
+  --output_dir outputs/V004 \
+  --do_tsne --do_margin --do_scale_vis >> logs/V004.log 2>&1
